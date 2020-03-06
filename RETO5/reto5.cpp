dbb3e4aed40da93714e71a56139d057d23a4c395
luisuvmmm <luis539346@gmail.com>
2020-03-06 18:25:54 +0000

    en proceso reto 5 y 6

---
+++ RETO5/reto5.cpp
@@ -0,0 +1,24 @@
+#include <iostrem>
+using namespace std;
+int minsaltos(int X, int Y, int D){
+   int res;
+   int diff = Y-X;
+   return res;
+
+
+
+}
+
+int main(){
+ int X, Y, D;
+ int Resultado;
+ cout<<"Parcero, punto de inicio:"<<endl;
+ cin>> X;
+ cout<<"Parcero, punto de fin:"<<endl;
+ cin>> Y;
+ cout<<"Parcero, punto de distancia:"<<endl;
+ cin>> D;
+ Resultado = minsaltos(X, Y, D);
+ cout<<"Parcero, saltos necesarios minimos de Wonejo"<<Resultados;
+
+}
---
+++ RETO6/6.cpp
@@ -0,0 +1,24 @@
+#include<iostrem>
+#include<algorithm>
+#include<vector>
+
+
+using namespace std;
+
+int solution(vector<int> A){
+    orden = sort{our_vector.begin(),A,end());
+    cout << orden [1];
+    cout << orden [2];
+    cout << orden [3];
+    cout << orden [4];
+    cout << orden [5];
+    cout << orden [6];
+}
+
+int main(){
+    vector <int> A={1,2,3,4,5,6};
+    faltante = solution();
+    cout <<"parcero, tu numero faltante es:"<<faltante;
+    
+    
+}
